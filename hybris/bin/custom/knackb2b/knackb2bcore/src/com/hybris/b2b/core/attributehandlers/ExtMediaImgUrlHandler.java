078f10dcfd1e540a057f6b35ac403ce94cee4000
Bharath <bharath.yelugula@knacksystems.com>
2018-03-15 23:17:47 +0530

    Commit Grunt, less files - Knack Template changes

--- hybris/bin/custom/knackb2b/knackb2bcore/src/com/hybris/b2b/core/attributehandlers/ExtMediaImgUrlHandler.java
+++ hybris/bin/custom/knackb2b/knackb2bcore/src/com/hybris/b2b/core/attributehandlers/ExtMediaImgUrlHandler.java
@@ -7,6 +7,9 @@ import de.hybris.platform.core.model.product.ProductModel;
 import de.hybris.platform.servicelayer.model.attribute.DynamicAttributeHandler;
 import de.hybris.platform.util.Config;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +22,14 @@ import java.util.Map;
  */
 public class ExtMediaImgUrlHandler implements DynamicAttributeHandler<Map, ProductModel>
 {
+	public static final String SCALE_H200W300 = "c_scale,h_200,w_300";
+	public static final String SCALE_H1200W1200 = "c_scale,h_1200,w_1200";
+	public static final String SCALE_H512W512 = "c_scale,h_512,w_512";
+	public static final String SCALE_H246W365 = "c_scale,h_246,w_365";
+	public static final String SCALE_H300W300 = "c_scale,h_300,w_300";
+	public static final String SCALE_H96W96 = "c_scale,h_96,w_96";
+	public static final String SCALE_H65W65 = "c_scale,h_65,w_65";
+	public static final String SCALE_H30W30 = "c_scale,h_30,w_30";
 
 	/*
 	 * (non-Javadoc)
@@ -33,22 +44,17 @@ public class ExtMediaImgUrlHandler implements DynamicAttributeHandler<Map, Produ
 		/*
 		 * #https://res.cloudinary.com/<cloud_name>/<resource_type>/<type>/<version>/<public_id>.<format>
 		 * #http://res.cloudinary.com/knackhy/image/upload/c_scale,h_200,w_300/v1516652821/sunflower.jpg
-		 *
 		 * product.imagemgmt.service=cloudinary product.imagemgmt.prefixurl=https://res.cloudinary.com/
 		 * product.imagemgmt.cloudname=knackhy product.imagemgmt.resourceType=image product.imagemgmt.type=upload
 		 * product.imagemgmt.version=v1516391540 product.imagemgmt.imgFormat=jpg
 		 */
 		final String scale = "c_scale,h_200,w_300";
-		final String filename = prodModel.getCode() + "." + Config.getString("product.imagemgmt.imgFormat", "");
+		final String filename = prodModel.getExtImageFileName();// this should get the extension also ..
+		//final String filename = prodModel.getCode() + "." + Config.getString("product.imagemgmt.imgFormat", "");
 		final String urlString = Config.getString("product.imagemgmt.prefixurl", "") + "/"
 				+ Config.getString("product.imagemgmt.cloudname", "") + "/" + Config.getString("product.imagemgmt.resourceType", "")
 				+ "/" + Config.getString("product.imagemgmt.type", "") + "/" + scale + "/" + filename;
 		System.out.println("new product url is" + urlString + "with product name as" + prodModel.getName());
-		/*
-		 * try { final URL url = new URL(urlString); if (ImageIO.read(url).getWidth() > 0) { return urlString; } else {
-		 * return null; } } catch (final IOException e) { // YTODO Auto-generated catch block e.printStackTrace(); return
-		 * null; }
-		 */
 		/*
 		 * "superZoom" - "1200Wx1200H" "zoom" - "515Wx515H" "store" -"365Wx246H" "product" - "300Wx300H" "thumbnail" -
 		 * "96Wx96H" "cartIcon" - "65Wx65H" "styleSwatch" - "30Wx30H"
@@ -56,13 +62,47 @@ public class ExtMediaImgUrlHandler implements DynamicAttributeHandler<Map, Produ
 		//not to disturb he current flow...
 		final Map prodUrls = new HashMap();
 		final List gallery = new ArrayList();
-		prodUrls.put("superZoom", this.buildUrlString(filename, "c_scale,h_1200,w_1200"));
-		prodUrls.put("zoom", this.buildUrlString(filename, "c_scale,h_512,w_512"));
-		prodUrls.put("store", this.buildUrlString(filename, "c_scale,h_246,w_365"));
-		prodUrls.put("product", this.buildUrlString(filename, "c_scale,h_300,w_300"));
-		prodUrls.put("thumbnail", this.buildUrlString(filename, "c_scale,h_96,w_96"));
-		prodUrls.put("cartIcon", this.buildUrlString(filename, "c_scale,h_65,w_65"));
-		prodUrls.put("styleSwatch", this.buildUrlString(filename, "c_scale,h_30,w_30"));
+		String url = this.buildUrlString(filename, SCALE_H1200W1200);
+		if (urlValidator(url))
+		{
+			prodUrls.put("superZoom", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H512W512);
+		if (urlValidator(url))
+		{
+			prodUrls.put("zoom", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H246W365);
+		if (urlValidator(url))
+		{
+			prodUrls.put("store", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H300W300);
+		if (urlValidator(url))
+		{
+			prodUrls.put("product", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H96W96);
+		if (urlValidator(url))
+		{
+			prodUrls.put("thumbnail", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H65W65);
+		if (urlValidator(url))
+		{
+			prodUrls.put("cartIcon", url);
+		}
+		url = null;
+		url = this.buildUrlString(filename, SCALE_H30W30);
+		if (urlValidator(url))
+		{
+			prodUrls.put("styleSwatch", url);
+		}
 		prodUrls.put("gallery", gallery);
 		return prodUrls;
 	}
@@ -93,6 +133,25 @@ public class ExtMediaImgUrlHandler implements DynamicAttributeHandler<Map, Produ
 
 	}
 
+	public boolean urlValidator(final String url)
+	{
+		try
+		{
+			new URL(url).toURI();
+			return true;
+		}
+		catch (final URISyntaxException exception)
+		{
+			exception.printStackTrace();
+			return false;
+		}
+		catch (final MalformedURLException exception)
+		{
+			exception.printStackTrace();
+			return false;
+		}
+
+	}
 	/*
 	 * (non-Javadoc)
 	 *
--- hybris/bin/custom/knackb2b/knackb2bstorefront/Gruntfile.js
+++ hybris/bin/custom/knackb2b/knackb2bstorefront/Gruntfile.js
@@ -21,51 +21,139 @@ module.exports = function(grunt) {
             tasks: ['sync:syncjquery'],
         },
     },
-    less: {
-        default: {
+    
+ less: { 
+ 
+  knack1: {
             files: [
                 {
                     expand: true,
                     cwd: 'web/webroot/WEB-INF/_ui-src/',
-                    src: '**/themes/**/less/style.less',
+     src: '**/themes/**/less/knackstyleheader1.less',
                     dest: 'web/webroot/_ui/',
                     ext: '.css',
                     rename:function(dest,src){
                        var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
                        return dest+nsrc;
                     }
+   
+                }
+            ]
+        },
+  
+  knack2: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'web/webroot/WEB-INF/_ui-src/',
+     src: '**/themes/**/less/knackstyleheader2.less',
+                    dest: 'web/webroot/_ui/',
+                    ext: '.css',
+                    rename:function(dest,src){
+                       var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
+                       return dest+nsrc;
+                    }
+   
+                }
+            ]
+        },
+  
+  knack3: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'web/webroot/WEB-INF/_ui-src/',
+     src: '**/themes/**/less/knackstyleheader3.less',
+                    dest: 'web/webroot/_ui/',
+                    ext: '.css',
+                    rename:function(dest,src){
+                       var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
+                       return dest+nsrc;
+                    }
+   
+                }
+            ]
+        },
+  
+  knack4: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'web/webroot/WEB-INF/_ui-src/',
+     src: '**/themes/**/less/knackstyleheader4.less',
+                    dest: 'web/webroot/_ui/',
+                    ext: '.css',
+                    rename:function(dest,src){
+                       var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
+                       return dest+nsrc;
+                    }
+   
+                }
+            ]
+        },
+  
+  knack5: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'web/webroot/WEB-INF/_ui-src/',
+     src: '**/themes/**/less/knackstyleheader5.less',
+                    dest: 'web/webroot/_ui/',
+                    ext: '.css',
+                    rename:function(dest,src){
+                       var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
+                       return dest+nsrc;
+                    }
+   
+                }
+            ]
+        },
+  
+  default: {
+            files: [
+                {
+                    expand: true,
+                    cwd: 'web/webroot/WEB-INF/_ui-src/',
+     src: '**/themes/**/less/style.less',
+                    dest: 'web/webroot/_ui/',
+                    ext: '.css',
+                    rename:function(dest,src){
+                       var nsrc = src.replace(new RegExp("/themes/(.*)/less"),"/theme-$1/css");
+                       return dest+nsrc;
+                    }
+   
                 }
             ]
         },
     },
 
     sync : {
-    	syncfonts: {
-    		files: [{
+     syncfonts: {
+      files: [{
                 expand: true,
-    			cwd: 'web/webroot/WEB-INF/_ui-src/',
-    			src: '**/themes/**/fonts/*',
-    			dest: 'web/webroot/_ui/',
-    			rename:function(dest,src){
-                	var nsrc = src.replace(new RegExp("/themes/(.*)"),"/theme-$1");
-                	return dest+nsrc;
+       cwd: 'web/webroot/WEB-INF/_ui-src/',
+       src: '**/themes/**/fonts/*',
+       dest: 'web/webroot/_ui/',
+       rename:function(dest,src){
+                 var nsrc = src.replace(new RegExp("/themes/(.*)"),"/theme-$1");
+                 return dest+nsrc;
              }
-    		}]
-    	},
-    	syncybase: {
-    		files: [{
-    			cwd: 'web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/',
-    			src: '**/*.js',
-    			dest: 'web/webroot/_ui/responsive/common/js',
-    		}]
-    	},
-    	syncjquery: {
-    		files: [{
-    			cwd: 'web/webroot/WEB-INF/_ui-src/responsive/lib',
-    			src: 'jquery*.js',
-    			dest: 'web/webroot/_ui/responsive/common/js',
-    		}]
-    	}
+      }]
+     },
+     syncybase: {
+      files: [{
+       cwd: 'web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/',
+       src: '**/*.js',
+       dest: 'web/webroot/_ui/responsive/common/js',
+      }]
+     },
+     syncjquery: {
+      files: [{
+       cwd: 'web/webroot/WEB-INF/_ui-src/responsive/lib',
+       src: 'jquery*.js',
+       dest: 'web/webroot/_ui/responsive/common/js',
+      }]
+     }
     }
     
 });
@@ -75,10 +163,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-sync');
 
-
+  // Knack task(s).
+  grunt.registerTask('knack1', ['less', 'sync']);
+  grunt.registerTask('knack2', ['less', 'sync']);
+  grunt.registerTask('knack3', ['less', 'sync']);
+  grunt.registerTask('knack4', ['less', 'sync']);
+  grunt.registerTask('knack5', ['less', 'sync']);
   // Default task(s).
   grunt.registerTask('default', ['less', 'sync']);
-
-
-
+  
 };
